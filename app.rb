21e3a091428a74b38fac0a57396e325e27ab01ca
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-18 15:04:26 +0000

    update-for-rake-grade-review

--- app.rb
+++ app.rb
@@ -2,8 +2,51 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:nav)
+  erb(:home, {:layout => :layout})
+end
+
+rps = ["rock", "paper", "scissors"]
+@they = rps.sample
+
+#Play rock!
+get("/rock") do
+  erb(:nav, {:layout=>:layout})
+  if @they == "rock"
+    @outcome = "We tied!"
+  elsif @they == "paper"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+  erb(:rock, {:layout => :layout})
+  erb(:rules, {:layout=>:layout})
+end
+
+#Play paper!
+get("/paper") do
+  erb(:nav, {:layout=>:layout})
+  if @they == "rock"
+    @outcome = "We won!"
+  elsif @they == "paper"
+    @outcome = "We tied!"
+  else
+    @outcome = "We lost!"
+  end
+  erb(:paper, {:layout => :layout})
+  erb(:rules, {:layout=>:layout})
+end
+
+#Play scissors!
+get("/scissors") do
+  erb(:nav, {:layout=>:layout})
+  if @they == "rock"
+    @outcome = "We lost!"
+  elsif @they == "paper"
+    @outcome = "We won!"
+  else
+    @outcome = "We tied!"
+  end
+  erb(:scissors, {:layout => :layout})
+  erb(:rules, {:layout=>:layout})
 end
